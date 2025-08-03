b96b9e7cbdc35629100139b432de1198cf2443cb
Lucas G. Bonato <lucasbon4to@gmail.com>
2025-08-03 03:08:30 -0300

    separate json builder into a method

--- JsonCsvConverter.Cli/Converter.cs
+++ JsonCsvConverter.Cli/Converter.cs
@@ -19,33 +19,16 @@ namespace JsonCsvConverter.Cli
             }
 
             Console.WriteLine($"Converting '{csvFilePath}'...");
-
-            // TODO: Implement code abstraction 
-            // to make the code more readable**
-
             string csvContent = File.ReadAllText(csvFilePath);
 
-            // Separate lines and headers
-            string[] lines = csvContent.Split("\n\n")[0] // "\n\n" separates the ghost lines
-                .Split('\n');
-
-            string[] headerCells = lines[0].Split(';');
-
-            // Starts the file content
-            string jsonContent = "[";
-
-            for (int i = 1; i < lines.Length; i++)
+            // Creates the json content
+            var (jsonContent, error) = JsonBuilder(csvContent, hasHeader: true);
+            if (error != null)
             {
-                string[] lineCells = lines[i].Split(';');
-
-                for (int j = 0; j < headerCells.Length; j++)
-                {
-                    if (j == 0) { jsonContent += "{"; }
-                    jsonContent += $"\"{headerCells[j]}\": \"{lineCells[j]}\"";
-                    if (j == headerCells.Length - 1) { jsonContent += "}"; } else { jsonContent += ", "; }
-                }
-
-                if (i != lines.Length - 1) { jsonContent += ", "; } else { jsonContent += "]"; }
+                // I know it's not the best thing to use a 'golang-way' error handling here... 
+                // but who knows, maybe it'll stay like that :D
+                System.Console.WriteLine($"Error: {error}");
+                return "";
             }
 
             // Changes the file extension
@@ -54,6 +37,42 @@ namespace JsonCsvConverter.Cli
             File.WriteAllText(jsonFilePath, jsonContent);
 
             return jsonFilePath;
+        }
+
+        private static (string jsonContent, Exception? error) JsonBuilder(string csvContent, bool hasHeader)
+        {
+            // Not possible to convert (i guess)
+            if (!hasHeader) { return ("", new Exception("No header found in CSV file.")); }
+
+            // Separate lines and headers
+            string[] lines = csvContent.Split("\n\n")[0] // "\n\n" separates the ghost lines
+                .Split('\n');
+
+            string[] headerCells = lines[0].Split(';');
+            // Starts the file content
+            string jsonContent = "[";
+            try
+            {
+                for (int i = 1; i < lines.Length; i++)
+                {
+                    string[] lineCells = lines[i].Split(';');
+
+                    for (int j = 0; j < headerCells.Length; j++)
+                    {
+                        if (j == 0) { jsonContent += "{"; }
+                        jsonContent += $"\"{headerCells[j]}\": \"{lineCells[j]}\"";
+                        if (j == headerCells.Length - 1) { jsonContent += "}"; } else { jsonContent += ", "; }
+                    }
+
+                    if (i != lines.Length - 1) { jsonContent += ", "; } else { jsonContent += "]"; }
+                }
+            }
+            catch (Exception error)
+            {
+                return ("", error);
+            }
+
+            return (jsonContent, null);
         }
     }
 }
